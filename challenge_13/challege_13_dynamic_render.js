fccd54fc46894cefa499c313f13caf80ecd3d5bd
Ankan1998 <ankanroni3@gmail.com>
2021-11-30 21:07:32 +0530

    customizing views directory

--- challenge_13/challege_13_dynamic_render.js
+++ challenge_13/challege_13_dynamic_render.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Handlebar template engine setup
 app.set('view engine', 'hbs')
+viewPath = path.join(__dirname,'/views')
+app.set('views',viewPath)
 
 app.use(express.static(path.join(__dirname,'/public')))
 
 const forecast  = require('../challenge_9/challenge_9_callback_abstraction')
-const k = ''
+const k = '11820ab3394efd0ba19ca8e4ae1897cf'
 forecast(-75.7088, 44.1545,k ,(error, data) => {
     console.log('Error', error);
     console.log(data); 
