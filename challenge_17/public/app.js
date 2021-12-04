8e418574c7c4829added5008d1a87bedaea158a1
Ankan1998 <ankanroni3@gmail.com>
2021-12-04 14:17:47 +0530

    wiring up with js and paragraph element

--- challenge_17/public/app.js
+++ challenge_17/public/app.js
@@ -6,18 +6,19 @@ const config = {
 const weatherform = document.querySelector('form')
 const long = document.querySelector('input.long')
 const lat = document.querySelector('input.lat')
+const result = document.querySelector('p.result')
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault()
     fetch('http://api.weatherstack.com/current?access_key='+config.api_key+'&query=' + lat.value + ',' + long.value)
     .then((response) => {
         response.json().then((data)=>{
             if(data.error){
-                console.log(data.error);
+                result.innerHTML = "Error "+ data.error
             } else {
-                console.log({
-                    current_temp:data.current.temperature,
-                    apparent_temp:data.current.feelslike,
-                })
+                
+                    result.innerHTML = "Current Temperature: "+ data.current.temperature
+                    + "; Apparent temperature: " + data.current.feelslike ;
+                    
             }
         })
     })
