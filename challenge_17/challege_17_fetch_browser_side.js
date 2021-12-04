de3f35fe0f31811a02beb98e28bb730087ab3393
Ankan1998 <ankanroni3@gmail.com>
2021-12-04 13:17:20 +0530

    challenge 17 fetch

---
+++ challenge_17/challege_17_fetch_browser_side.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Handlebar template engine setup
+app.set('view engine', 'hbs')
+viewPath = path.join(__dirname,'/views')
+app.set('views',viewPath)
+
+app.use(express.static(path.join(__dirname,'/public')))
+
+
+app.get("/contact",(req,res)=>{
+    res.render('contact');
+    
+}) 
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(5000,()=>{
+    console.log("Server up and running at 5000")
+})
