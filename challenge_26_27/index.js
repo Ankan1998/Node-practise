55c39c6439757dc929f5d54f0925e503d7e182dd
Ankan1998 <ankanroni3@gmail.com>
2021-12-06 21:48:13 +0530

    findOne added

--- challenge_26_27/index.js
+++ challenge_26_27/index.js
@@ -24,6 +24,32 @@ app.get('/users',(req,res)=>{
     })
 })
 
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/email/:email',(req,res)=>{
+    const email_id = req.params.email
+    console.log(email_id)
+    User.findOne({email: email_id}).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
 app.listen(port,()=>{
     console.log('Server listening on port: ' + port)
 })
