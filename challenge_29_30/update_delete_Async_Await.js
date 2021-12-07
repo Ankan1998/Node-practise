6f2eaeba7e9c3c550ce9aac061eded7210a68542
Ankan1998 <ankanroni3@gmail.com>
2021-12-07 15:15:26 +0530

    delete added

--- challenge_29_30/update_delete_Async_Await.js
+++ challenge_29_30/update_delete_Async_Await.js
@@ -36,6 +36,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch(e) {
+        res.status(400).send()
+    }
+})
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port)
