a67d7938deb15f206096567f75886aef062f3749
Ankan1998 <ankanroni3@gmail.com>
2021-12-09 17:21:07 +0530

    passing data as query from node js and using in python

--- challenge_33/index.js
+++ challenge_33/index.js
@@ -12,7 +12,17 @@ app.get("/python",(req,res)=>{
        
 })  
 
-  
+app.post("/python",(req,res)=>{
+    if(Object.keys(req.query).length == 0){
+        return res.status(400).send("Error: Add Query")
+    }
+    const py = spawn('python', ['./challenge_33/pydata.py',req.query.id]);
+    py.stdout.on('data',function(data){
+        console.log(data.toString())
+        res.send(data)
+    })
+       
+})  
 
 app.get("/",(req,res)=>{
     res.send('<h1>Welcome</h1>');
