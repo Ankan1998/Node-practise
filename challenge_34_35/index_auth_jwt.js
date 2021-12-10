1ee88e6f7c80a7388b203c42e97c5c3b102d3506
Ankan1998 <ankanroni3@gmail.com>
2021-12-11 00:07:07 +0530

    middleware at server side added

--- challenge_34_35/index_auth_jwt.js
+++ challenge_34_35/index_auth_jwt.js
@@ -4,6 +4,11 @@ const UserRouter = require('./router/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+// Middleware added to show server down
+app.use((req,res,next)=>{
+    res.status(503).send("Server under Maintenance")
+})
+
 app.use(express.json())
 app.use(UserRouter)
 
