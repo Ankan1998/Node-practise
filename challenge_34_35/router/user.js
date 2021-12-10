207e6b6163a1f618fa9921802d18446e4d04f97c
Ankan1998 <ankanroni3@gmail.com>
2021-12-10 21:26:16 +0530

    signup linkd with web token generation

---
+++ challenge_34_35/router/user.js
@@ -0,0 +1,83 @@
+const express = require('express')
+require('../mongoose')
+const User = require('../model')
+const router = new express.Router()
+
+const app = express()
+
+app.use(express.json())
+
+router.post('/users',async (req,res)=>{
+    const user = new User(req.body)
+    try {
+        await user.save()
+        const token = await user.generateToken(user._id)
+        res.status(201).send({user,token})
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+router.patch('/users/:id', async (req, res) => {
+    const changes = Object.keys(req.body) // Convert json fields to array 
+    const allowedChanges = ['name','email','password']
+    const isValidChange = changes.every((change)=>{
+        return allowedChanges.includes(change)
+    })
+    if(!isValidChange){
+        return res.status(400).send()
+    }
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        changes.forEach((change)=>{
+            user[change] = req.body[change]
+        })
+        
+        await user.save()
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch(e) {
+        res.status(400).send()
+    }
+})
+
+router.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+        return User.countDocuments({})
+    }).then((result)=>{
+        console.log(result)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+router.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+router.post('/users/login',async (req,res)=>{
+    try{
+        const user =  await User.matchCred(req.body.mobileNo,req.body.password)
+        return res.send(user).status(200)
+    } catch(e){
+        res.status(400).send()
+    }
+})
+
+module.exports = router;
