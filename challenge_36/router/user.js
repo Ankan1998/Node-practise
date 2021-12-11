663292f2d1d5dfd08f15edd94f4cbddef6665f00
Ankan1998 <ankanroni3@gmail.com>
2021-12-11 12:43:45 +0530

    user logout endpoint added

--- challenge_36/router/user.js
+++ challenge_36/router/user.js
@@ -8,82 +8,107 @@ const app = express()
 
 app.use(express.json())
 
-router.get('/users/me',auth,async (req,res)=>{
+// User profile 
+router.get('/users/me', auth, async (req, res) => {
     return res.send(req.user)
 })
 
-router.post('/users',async (req,res)=>{
+// new user signup
+router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
         const token = await user.generateToken(user._id)
-        res.status(201).send({user,token})
+        res.status(201).send({
+            user,
+            token
+        })
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
+//User login
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.matchCred(req.body.mobileNo, req.body.password)
+        const token = await user.generateToken(user._id)
+        return res.status(200).send({
+            user,
+            token
+        })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        req.user.save()
+        res.send("User logged out")
+    } catch(e) {
+        res.status(400).send()
+    }
+
+})
+
+//Update user
 router.patch('/users/:id', async (req, res) => {
     const changes = Object.keys(req.body) // Convert json fields to array 
-    const allowedChanges = ['name','email','password']
-    const isValidChange = changes.every((change)=>{
+    const allowedChanges = ['name', 'email', 'password']
+    const isValidChange = changes.every((change) => {
         return allowedChanges.includes(change)
     })
-    if(!isValidChange){
+    if (!isValidChange) {
         return res.status(400).send()
     }
     const _id = req.params.id
     try {
         const user = await User.findById(_id)
-        changes.forEach((change)=>{
+        changes.forEach((change) => {
             user[change] = req.body[change]
         })
-        
+
         await user.save()
-        if(!user){
+        if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
 
-    } catch(e) {
+    } catch (e) {
         res.status(400).send()
     }
 })
 
-router.get('/users',(req,res)=>{
-    User.find({}).then((users)=>{
+// get all user
+router.get('/users', (req, res) => {
+    User.find({}).then((users) => {
         res.send(users)
         return User.countDocuments({})
-    }).then((result)=>{
+    }).then((result) => {
         console.log(result)
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(500).send()
     })
 })
 
-router.get('/users/:id',(req,res)=>{
+// Get user by id
+router.get('/users/:id', (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user)=>{
-        if(!user){
+    User.findById(_id).then((user) => {
+        if (!user) {
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e)=>{
+    }).catch((e) => {
         res.status(500).send(e)
     })
 })
 
-router.post('/users/login',async (req,res)=>{
-    try{
-        const user =  await User.matchCred(req.body.mobileNo,req.body.password)
-        const token = await user.generateToken(user._id)
-        return res.status(200).send({user,token})
-    } catch(e){
-        res.status(400).send()
-    }
-})
 
 module.exports = router;
