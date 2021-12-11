63b31b3203e5858e6aaa4e2085d0388b908d74a9
Ankan1998 <ankanroni3@gmail.com>
2021-12-11 18:24:02 +0530

    task router added

--- challenge_37/index_user_task_relation.js
+++ challenge_37/index_user_task_relation.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const UserRouter = require('./router/user')
+const TaskRouter = require('./router/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(UserRouter)
+app.use(TaskRouter)
 
 app.listen(port,()=>{
     console.log('Server listening on port: ' + port)
