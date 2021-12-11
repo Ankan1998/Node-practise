e93886d56eef8682446411e3f5e6cc7a6492b2f7
Ankan1998 <ankanroni3@gmail.com>
2021-12-11 12:23:59 +0530

    minor update in request header

--- challenge_36/middleware/user_middleware.js
+++ challenge_36/middleware/user_middleware.js
@@ -3,7 +3,7 @@ const User = require("../model")
 
 const auth = async function(req,res,next){
     try {
-        const token = (req.headers.auth).split(' ')[1]
+        const token = (req.headers.authorization).split(' ')[1]
         const decoded_token = jwt.verify(token,'ankan123')
         const user = await User.findOne({_id:decoded_token._id,"tokens.token":token})
         if (!user){
