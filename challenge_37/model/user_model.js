339160329abedd689bde30de2de94f9743e23b1e
Ankan1998 <ankanroni3@gmail.com>
2021-12-11 16:09:04 +0530

    code refactored

---
+++ challenge_37/model/user_model.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true,
+        trim:true
+    },
+    email: {
+        type: String,
+        validate(val){
+            if(!validator.isEmail(val)){
+                throw new Error('Email invalid')
+            }
+        }
+    },
+    mobileNo:{
+        type:String,
+        unique:true,
+        validate(val){
+            if(!validator.isMobilePhone(val,['en-IN'])){
+                throw new Error('Mobile Number Invalid')
+            }
+        }
+    },
+    password:{
+        type: String
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true,
+        }
+    }]
+
+})
+
+// Hiding password and token
+UserSchema.methods.toJSON = function(){
+    const Obj = this.toObject()
+    delete Obj.password
+    delete Obj.tokens
+    return Obj
+}
+
+//Json web token instance method
+UserSchema.methods.generateToken = async function(id){
+    const token = jwt.sign({_id:id.toString()},"ankan123")
+    this.tokens.push({token:token})
+    await this.save()
+    return token
+}
+
+// Match credential static method(class method)
+UserSchema.statics.matchCred = async function(mobNo,password){
+    const user = await users.findOne({mobileNo:mobNo})
+    if(!user){
+        throw new Error("Login Error")
+    }
+    const isSame = await bcrypt.compare(password,user.password)
+    if(!isSame){
+        throw new Error("Login Error")
+    }
+    return user
+    
+}
+
+UserSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,8)
+    }
+    next()
+})
+
+const users = mongoose.model('User', UserSchema)
+
+module.exports = users
---
+++ challenge_37/router/user.js
@@ -0,0 +1,123 @@
+const express = require('express')
+require('../mongoose')
+const User = require('../model/user_model')
+const auth = require('../middleware/user_middleware')
+const router = new express.Router()
+
+const app = express()
+
+app.use(express.json())
+
+// User profile 
+router.get('/users/me', auth, async (req, res) => {
+    return res.send(req.user)
+})
+
+// new user signup
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        const token = await user.generateToken(user._id)
+        res.status(201).send({
+            user,
+            token
+        })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//User login
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.matchCred(req.body.mobileNo, req.body.password)
+        const token = await user.generateToken(user._id)
+        return res.status(200).send({
+            user,
+            token
+        })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// User logout
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        req.user.save()
+        res.send("User logged out")
+    } catch(e) {
+        res.status(400).send()
+    }
+
+})
+
+//User logout All
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        req.user.save()
+        res.send("User logged out fromm All places")
+    } catch(e) {
+        res.status(400).send()
+    }
+
+})
+
+//Update user
+router.patch('/users/update', auth, async (req, res) => {
+    const changes = Object.keys(req.body) // Convert json fields to array 
+    const allowedChanges = ['name', 'email', 'password']
+    const isValidChange = changes.every((change) => {
+        return allowedChanges.includes(change)
+    })
+    if (!isValidChange) {
+        return res.status(400).send()
+    }
+    try {
+        changes.forEach((change) => {
+            req.user[change] = req.body[change]
+        })
+
+        await req.user.save()
+        if (!req.user) {
+            return res.status(404).send()
+        }
+
+        res.send(req.user)
+
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// delete user
+router.delete('/users/delete', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        res.send(user)
+
+    } catch(e) {
+        res.status(400).send()
+    }
+})
+
+// get all user
+router.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+        return User.countDocuments({})
+    }).then((result) => {
+        console.log(result)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
+
+module.exports = router;
