eedc951c7736fa9631328a01f441f5a11c162d01
Ankan1998 <ankanroni3@gmail.com>
2021-12-14 22:05:09 +0530

    connections

---
+++ challenge_39/middleware/user_middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken")
+const User = require("../model/user_model")
+
+const auth = async function(req,res,next){
+    try {
+        const token = (req.headers.authorization).split(' ')[1]
+        const decoded_token = jwt.verify(token,'ankan123')
+        const user = await User.findOne({_id:decoded_token._id,"tokens.token":token})
+        if (!user){
+            throw new Error()
+        } 
+        req.user = user
+        req.token = token
+        next()
+    } catch(e){
+        res.status(400).send("PLease Authenticate")
+    }
+}
+
+module.exports = auth
